70e157222da08b48bf15c57077722fb612173540
GUERMITICHAWKI <Guermitichawki@gmail.com>
2025-12-12 03:58:15 +0100

    interface Swing gestion vols (main + vues)

--- src/flight/managementt/system/FlightManagementtSystem.java
+++ src/flight/managementt/system/FlightManagementtSystem.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package flight.managementt.system;
 
-/**
- *
- * @author CHAWKI
- */
+import view.NewJFrame;
+
 public class FlightManagementtSystem {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);   // mets ici le nom de ta JFrame
+            }
+        });
     }
-    
 }
