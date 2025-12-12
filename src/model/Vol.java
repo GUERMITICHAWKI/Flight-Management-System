cd21703908ee0cfe491b4f0d1277deb3b5546cec
GUERMITICHAWKI <Guermitichawki@gmail.com>
2025-12-12 23:46:59 +0100

    fix bugs

--- src/model/Vol.java
+++ src/model/Vol.java
@@ -2,90 +2,115 @@ package model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Vol {
 
-    private int id;
+    private Integer id;
     private LocalDate dateDepart;
     private LocalTime heureDepart;
     private LocalDate dateArrivee;
     private LocalTime heureArrivee;
     private boolean reservable;
+
+    // relations (tu pourras les utiliser plus tard)
     private Aeroport aeroportDepart;
     private Aeroport aeroportArrivee;
-    private List<Escale> escales = new ArrayList<>();
 
     public Vol() {
     }
 
-    public Vol(int id,
-               LocalDate dateDepart, LocalTime heureDepart,
+    public Vol(Integer id, LocalDate dateDepart, LocalTime heureDepart,
                LocalDate dateArrivee, LocalTime heureArrivee,
-               boolean reservable,
-               Aeroport aeroportDepart, Aeroport aeroportArrivee) {
+               boolean reservable) {
         this.id = id;
         this.dateDepart = dateDepart;
         this.heureDepart = heureDepart;
         this.dateArrivee = dateArrivee;
         this.heureArrivee = heureArrivee;
         this.reservable = reservable;
-        this.aeroportDepart = aeroportDepart;
-        this.aeroportArrivee = aeroportArrivee;
     }
 
     public Vol(LocalDate dateDepart, LocalTime heureDepart,
                LocalDate dateArrivee, LocalTime heureArrivee,
-               boolean reservable,
-               Aeroport aeroportDepart, Aeroport aeroportArrivee) {
-        this.dateDepart = dateDepart;
-        this.heureDepart = heureDepart;
-        this.dateArrivee = dateArrivee;
-        this.heureArrivee = heureArrivee;
-        this.reservable = reservable;
-        this.aeroportDepart = aeroportDepart;
-        this.aeroportArrivee = aeroportArrivee;
+               boolean reservable) {
+        this(null, dateDepart, heureDepart, dateArrivee, heureArrivee, reservable);
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    // getters / setters
 
-    public LocalDate getDateDepart() { return dateDepart; }
-    public void setDateDepart(LocalDate dateDepart) { this.dateDepart = dateDepart; }
+    public Integer getId() {
+        return id;
+    }
 
-    public LocalTime getHeureDepart() { return heureDepart; }
-    public void setHeureDepart(LocalTime heureDepart) { this.heureDepart = heureDepart; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public LocalDate getDateArrivee() { return dateArrivee; }
-    public void setDateArrivee(LocalDate dateArrivee) { this.dateArrivee = dateArrivee; }
+    public LocalDate getDateDepart() {
+        return dateDepart;
+    }
 
-    public LocalTime getHeureArrivee() { return heureArrivee; }
-    public void setHeureArrivee(LocalTime heureArrivee) { this.heureArrivee = heureArrivee; }
+    public void setDateDepart(LocalDate dateDepart) {
+        this.dateDepart = dateDepart;
+    }
 
-    public boolean isReservable() { return reservable; }
-    public void setReservable(boolean reservable) { this.reservable = reservable; }
+    public LocalTime getHeureDepart() {
+        return heureDepart;
+    }
 
-    public Aeroport getAeroportDepart() { return aeroportDepart; }
-    public void setAeroportDepart(Aeroport aeroportDepart) { this.aeroportDepart = aeroportDepart; }
+    public void setHeureDepart(LocalTime heureDepart) {
+        this.heureDepart = heureDepart;
+    }
 
-    public Aeroport getAeroportArrivee() { return aeroportArrivee; }
-    public void setAeroportArrivee(Aeroport aeroportArrivee) { this.aeroportArrivee = aeroportArrivee; }
+    public LocalDate getDateArrivee() {
+        return dateArrivee;
+    }
 
-    public List<Escale> getEscales() { return escales; }
-    public void setEscales(List<Escale> escales) { this.escales = escales; }
+    public void setDateArrivee(LocalDate dateArrivee) {
+        this.dateArrivee = dateArrivee;
+    }
 
-    public void ajouterEscale(Escale e) {
-        escales.add(e);
+    public LocalTime getHeureArrivee() {
+        return heureArrivee;
+    }
+
+    public void setHeureArrivee(LocalTime heureArrivee) {
+        this.heureArrivee = heureArrivee;
+    }
+
+    public boolean isReservable() {
+        return reservable;
+    }
+
+    public void setReservable(boolean reservable) {
+        this.reservable = reservable;
+    }
+
+    public Aeroport getAeroportDepart() {
+        return aeroportDepart;
+    }
+
+    public void setAeroportDepart(Aeroport aeroportDepart) {
+        this.aeroportDepart = aeroportDepart;
+    }
+
+    public Aeroport getAeroportArrivee() {
+        return aeroportArrivee;
+    }
+
+    public void setAeroportArrivee(Aeroport aeroportArrivee) {
+        this.aeroportArrivee = aeroportArrivee;
     }
 
     @Override
     public String toString() {
-        return "Vol{" + "id=" + id +
+        return "Vol{" +
+                "id=" + id +
                 ", dateDepart=" + dateDepart +
                 ", heureDepart=" + heureDepart +
                 ", dateArrivee=" + dateArrivee +
                 ", heureArrivee=" + heureArrivee +
-                ", reservable=" + reservable + '}';
+                ", reservable=" + reservable +
+                '}';
     }
 }
